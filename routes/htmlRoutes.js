8a845a4bf40ec616db97861b5cc63aef608582d7
hausu <kennedymj2@gmail.com>
2019-08-06 14:06:21 -0500

    added sever and html routes

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,22 @@
+var path = require("path");
+
+module.exports = function(app) {
+    
+    app.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+    });  
+  
+    app.get("/contact", function(req, res) {
+      res.sendFile(path.join(__dirname, "../public/contact.html"));
+    });
+  
+    app.get("/portfolio", function(req, res) {
+      res.sendFile(path.join(__dirname, "../public/portfolio.html"));
+    });
+  
+    // If no matching route is found default to home
+    app.get("*", function(req, res) {
+      res.sendFile(path.join(__dirname, "../public/index.html"));
+    });
+    
+  };
---
+++ server.js
@@ -0,0 +1,30 @@
+// DEPENDENCIES
+// =============================================================================
+
+var express = require("express");
+
+
+// EXPRESS CONFIGURATION
+// =============================================================================
+
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+// ROUTER
+// =============================================================================
+
+require("./routes/htmlRoutes")(app);
+
+
+// LISTENER
+// =============================================================================
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+  });
+  
